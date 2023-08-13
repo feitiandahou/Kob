a30073ec10d0f44556c04eee2f663cdb25070056
feitiandahou <1740393870@qq.com>
2023-08-13 14:57:07 +0800

    实现微服务： 匹配系统(3)

---
+++ backendcloud/backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -0,0 +1,166 @@
+package com.kob.backend.consumer;
+
+import com.alibaba.fastjson.JSONObject;
+import com.kob.backend.consumer.utils.Game;
+import com.kob.backend.consumer.utils.JwtAuthentication;
+import com.kob.backend.mapper.RecordMapper;
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+@ServerEndpoint("/websocket/{token}")
+public class WebSocketServer {
+    //与线程安全有关的哈希表，将userID映射到相应用户的WebSocketServer
+    final public  static ConcurrentHashMap<Integer, WebSocketServer> users =  new ConcurrentHashMap<>();
+    //当前链接请求的用户
+    private User user;
+    //后端向前端发信息,每个链接用session维护
+    private Session session = null;
+    private static UserMapper userMapper;
+    public static RecordMapper recordMapper;
+
+    private static RestTemplate restTemplate;
+    private Game game = null;
+    private final static String addPlayerUrl = "http://127.0.0.1:3001/player/add/";
+    private final static String removePlayerUrl = "http://127.0.0.1:3001/player/remove/";
+
+    @Autowired
+    public void setUserMapper(UserMapper userMapper){
+        WebSocketServer.userMapper = userMapper;//静态变量访问要用类名访问
+    }
+    @Autowired
+    public void setRecordMapper(RecordMapper recordMapper){
+        WebSocketServer.recordMapper = recordMapper;
+    }
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate){
+        WebSocketServer.restTemplate = restTemplate;
+    }
+    @OnOpen
+    public void onOpen(Session session, @PathParam("token") String token) throws IOException {
+    //建立连接
+        System.out.println("connected!");
+        this.session = session;
+        //为了方便调试，初阶段只把token当成userId看
+        Integer userId = JwtAuthentication.getUserId(token);
+        this.user = userMapper.selectById(userId);
+        if(this.user != null){
+            users.put(userId,this);
+        }else{
+            this.session.close();
+        }
+    }
+
+    @OnClose
+    public void onClose(){
+        System.out.println("disconnected!");
+        if(this.user != null){
+            users.remove(this.user.getId());
+        }
+    }
+
+    public static void startGame(Integer aId, Integer bId){
+        User a = userMapper.selectById(aId), b= userMapper.selectById(bId);
+
+        Game game = new Game(13, 14, 20,a.getId(),b.getId());
+        game.createMap();
+        if(users.get(a.getId())!= null){
+            users.get(a.getId()).game = game;
+        }
+        if(users.get(b.getId())!= null){
+            users.get(b.getId()).game = game;
+        }
+        game.start();
+
+        JSONObject respGame = new JSONObject();
+        respGame.put("a_id",game.getPlayerA().getId());
+        respGame.put("a_sx",game.getPlayerA().getSx());
+        respGame.put("a_sy",game.getPlayerA().getSy());
+        respGame.put("b_id",game.getPlayerB().getId());
+        respGame.put("b_sx",game.getPlayerB().getSx());
+        respGame.put("b_sy",game.getPlayerB().getSy());
+        respGame.put("map", game.getG());
+
+
+        JSONObject respA = new JSONObject();
+        respA.put("event","start-matching");
+        respA.put("opponent_username", b.getUsername());
+        respA.put("opponent_photo", b.getPhoto());
+        respA.put("game",respGame);
+        if(users.get(a.getId())!= null){
+            users.get(a.getId()).sendMessage(respA.toJSONString());
+        }
+
+        JSONObject respB = new JSONObject();
+        respB.put("event", "start-matching");
+        respB.put("opponent_username", a.getUsername());
+        respB.put("opponent_photo", a.getPhoto());
+        respB.put("game", respGame);
+        if(users.get(b.getId())!= null){
+            users.get(b.getId()).sendMessage(respB.toJSONString());
+        }
+    }
+    private void startMatching(){
+        System.out.println("start Macthing");
+        MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+        data.add("user_id",this.user.getId().toString());
+        data.add("rating",this.user.getRating().toString());
+        restTemplate.postForObject(addPlayerUrl,data,String.class);
+    }
+    private void stopMatching(){
+        System.out.println("stop Matching");
+        MultiValueMap<String,String> data = new LinkedMultiValueMap<>();
+        data.add("user_id",this.user.getId().toString());
+        restTemplate.postForObject(removePlayerUrl, data, String.class);
+    }
+
+    private void move(int direction) {
+        if(game.getPlayerA().getId().equals(user.getId())) {
+            game.setNextStepA(direction);
+        }else if(game.getPlayerB().getId().equals(user.getId())){
+            game.setNextStepB(direction);
+        }
+    }
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        // 从Client接收消息
+        System.out.println("receive message!");
+        JSONObject data = JSONObject.parseObject(message);
+        String event = data.getString("event");
+        if("start-matching".equals(event)){
+            startMatching();
+        } else if ("stop-matching".equals(event)){
+            stopMatching();
+        } else if ("move".equals(event)){
+            move(data.getInteger("direction"));
+        }
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+    public void sendMessage(String message){
+        //异步通信要加上锁
+        synchronized (this.session){
+            try{
+                this.session.getBasicRemote().sendText(message);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
